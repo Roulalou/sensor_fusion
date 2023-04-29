dfcd71e514a1828175926a447cf01f0ea68e5187
Roulalou <sebastien.gios@student.uclouvain.be>
2023-04-29 13:40:21 +0200

    seems good :+1:

--- src/learn.erl
+++ src/learn.erl
@@ -6,7 +6,9 @@
 main() ->
     Vector = parse(),
     Pattern = analyze(Vector),
-    print_list(Pattern).
+    % print_list(Pattern),
+    Flow = regroup(Pattern),
+    print_list(Flow).
 
 
 analyze(Vector) ->
@@ -16,8 +18,7 @@ analyze(Vector, Pattern) ->
     case Vector of
         [] -> Pattern;
         [H|T] ->
-            if 
-            H < -1 ->
+            if H < -1 ->
                 analyze(T, lists:append(Pattern, ["neg"]));
             H > 1 ->
                 analyze(T, lists:append(Pattern, ["pos"]));
@@ -25,3 +26,25 @@ analyze(Vector, Pattern) ->
                 analyze(T, lists:append(Pattern, ["zero"]))
             end
     end.
+
+regroup(Pattern) ->
+    regroup(Pattern, []).
+
+regroup(Pattern, Flow) ->
+    case Pattern of
+        [] -> Flow;
+        [H|T] ->
+            if T == [] ->
+                regroup(T, lists:append(Flow, [H]));
+            true ->
+                [HT|_] = T,
+                Next = HT, % head tail
+                if H == Next ->
+                    regroup(T, Flow);
+                true ->
+                    regroup(T, lists:append(Flow, [H]))
+                end
+            end
+    end.
+
+    
