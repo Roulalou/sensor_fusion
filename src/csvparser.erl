1c0c7e4dd56d9c50f939c2981d18ba18970f2d4a
Roulalou <sebastien.gios@student.uclouvain.be>
2023-04-29 12:06:56 +0200

    one data

--- src/csvparser.erl
+++ src/csvparser.erl
@@ -1,10 +1,12 @@
 -module(csvparser).
 
--export([main/0, parser/0]).
+-export([main/0]).
 
 main() ->
-    % Lines = parser().
-    parser().
+    Lines = parser(),
+    CL = clean_lines(Lines),
+    print_csv(CL),
+    CL.
 
 parser() ->
     % {ok, File} = file:open("../measures/AFTERe11_sensor_fusion@nav_1.csv", [read]),
@@ -15,10 +17,24 @@ parser() ->
     % io:format("Data : ~p~n", [Data]),
     Rows = string:tokens(binary_to_list(Data), "\n"),
     Records = [string:tokens(Row, ",") || Row <- Rows],
-    print_csv(Records).
+    % print_csv(Records).
+    Records.
 
 print_csv([H|T]) ->
     io:format("H : ~p~n", [H]),
     print_csv(T);
 print_csv([]) ->
     ok.
+
+clean_lines([H|T]) ->
+    Just_AccX_H  = lists:nth(3, H),
+    clean_lines(T, [Just_AccX_H]).
+
+clean_lines(List, AccX) ->
+    case List of 
+        [] -> AccX;
+        [H|T] ->
+            Just_AccX_H  = lists:nth(3, H),
+            New_H = lists:append(AccX, [Just_AccX_H]),
+            clean_lines(T, New_H)
+    end.
