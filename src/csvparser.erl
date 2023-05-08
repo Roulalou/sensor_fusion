c751d13adbca4dcf39e7d60a65a8603b311be86e
Roulalou <sebastien.gios@student.uclouvain.be>
2023-05-08 16:05:29 +0200

    normalize Z

--- src/csvparser.erl
+++ src/csvparser.erl
@@ -4,12 +4,15 @@
 
 % CSV : "../measures/Anav3_sensor_fusion@nav_1.csv"
 % return a list of the column Index
-% For nav_3 acceleration are in 5, 8, 11
+% For nav_3 acceleration are in 3, 4, 5
 parse(CSV, Index) ->
     Lines = parser(CSV),
     CL = clean_lines(Lines, Index),
-    % print_list(CL),
-    CL.
+    if Index == 5 -> % HARDCODE for Z acceleration for nav3
+        z_accel_norm(CL);
+    true ->
+        CL
+    end.
 
 % return a list of the lines
 parser(CSV) ->
@@ -36,4 +39,18 @@ clean_lines(List, Index, AccX) ->
             Just_AccX_H  = lists:nth(Index, H),
             New_H = lists:append(AccX, [Just_AccX_H]),
             clean_lines(T, Index, New_H)
+    end.
+
+% add 9,81 to each value of the Z axis, to counter gravity
+z_accel_norm(List) ->
+    z_accel_norm(List, []).
+
+z_accel_norm(List, Cleaned) ->
+    case List of
+        [] -> Cleaned;
+        [H|T] ->
+            {Int_H, _} = string:to_float(H),
+            New_H = Int_H + 9.81,
+            New_Cleaned = lists:append(Cleaned, [New_H]),
+            z_accel_norm(T, New_Cleaned)
     end.
